14c5c5a3dd8f5657f6b251bf4b73ab93f73dd08f
clinton-hall <fock_wulf@hotmail.com>
2016-09-26 09:41:45 +0930

    fix shebang following merge.

--- passwordList.py
+++ passwordList.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 #
 ##############################################################################
 ### NZBGET POST-PROCESSING SCRIPT                                          ###
